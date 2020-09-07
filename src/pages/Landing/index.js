6bbbdef7812dd763e742bb17a0fdda681bf8b67c
Buzzgrow <artur_pg2011@hotmail.com>
2020-09-07 11:13:57 -0300

    start settings and landing page

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+import landingImg from '../../assets/images/hero.png';
+
+import './styles.css';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+
+        <div className="caption">
+          <h2>Buscando inspiração?</h2>
+          <h3>Descubra os melhores carros da internet!</h3>
+        </div>
+
+        <img src={landingImg} className="hero-image" alt="Carro" />
+
+        <div className="buttons-container">
+          <Link to="/car-list" className="car-list">
+            <i className="fas fa-stream" />
+            Listar carros
+          </Link>
+          
+          <Link to="/car-register" className="car-register">
+            <i className="fas fa-plus" />
+            Cadastrar Carros
+          </Link>
+        </div>
+
+        <span className="credits">
+					<p>Desenvolvido de <i class="fas fa-heart"></i> por
+          <a href="https://www.linkedin.com/in/freirart"> Artur Freire</a>.</p>
+				</span>
+
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
